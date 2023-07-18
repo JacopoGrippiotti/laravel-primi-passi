6b7f6614d16358184f183205cb888acc47beb626
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-07-18 16:51:05 +0200

    added a route to home.blade.php file and also data

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    
+    $data = [
+        'message' => 'Hello World',
+        'numbers' => [1,2,3]
+    ];
+    
+    return view('home', $data);
 });
